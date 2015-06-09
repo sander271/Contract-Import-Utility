148300d63d4a9175ac8f5a1f318c9b7a947a61ad
unknown <asanders@interndesktop1.autotask.com>
2015-06-09 13:18:51 -0400

    add https security

--- app/ajax.php
+++ app/ajax.php
@@ -72,7 +72,7 @@ uploadFile();
         }
     </style>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
     <script>
         function runBar(){
